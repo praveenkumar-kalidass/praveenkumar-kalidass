5151dd6e0d2809b670359b2b63409a73406a98a2
praveenkumar-outlook <praveen.ece@outlook.com>
2019-01-20 11:56:37 +0530

    Init : React starter

---
+++ production.config.babel.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: {
+    app: path.join(__dirname, "src/index.js")
+  },
+  output: {
+    path: path.join(__dirname, "public"),
+    hashDigestLength: 10,
+    filename: "scripts/[name].[hash].bundle.js"
+  },
+  mode: "production",
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.join(__dirname, "public/index.html"),
+      template: path.join(__dirname, "src/index.html")
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          babelrc: false,
+          plugins: [
+            "transform-class-properties"
+          ],
+          presets: [
+            "env",
+            "react"
+          ]
+        }
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        "sass-loader"
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        "css-loader"
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [{
+        loader: "file-loader",
+        options: {
+          name: "[hash].[ext]",
+          outputPath: "images/"
+        }
+      }]
+    }, {
+      test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
+      use: [
+        "file-loader"
+      ]
+    }]
+  }
+};
+
+module.exports = config;
---
+++ webpack.config.babel.js
@@ -0,0 +1,79 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: {
+    app: path.join(__dirname, "src/index.js")
+  },
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "scripts/[name].bundle.js"
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.join(__dirname, "public/index.html"),
+      template: path.join(__dirname, "src/index.html")
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    historyApiFallback: true,
+    hot: true,
+    open: true
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          babelrc: false,
+          plugins: [
+            "transform-class-properties"
+          ],
+          presets: [
+            "env",
+            "react"
+          ]
+        }
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        "sass-loader"
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        "css-loader"
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [{
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "images/"
+        }
+      }]
+    }, {
+      test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
+      use: [
+        "file-loader"
+      ]
+    }]
+  }
+};
+
+module.exports = config;
