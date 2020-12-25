b1214227811b0b73f7afbcc8fa58951e768c7b48
Praveenkumar Kalidass <praveen.ece@outlook.com>
2020-12-25 17:23:35 +0530

    App: Include Contact page

--- praveenkumar-kalidass/src/App/App.js
+++ praveenkumar-kalidass/src/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import About from '../page/About';
+import Contact from '../page/Contact';
 import Home from '../page/Home';
 
 const App = () => (
   <>
     <Home />
     <About />
+    <Contact />
   </>
 );
 
