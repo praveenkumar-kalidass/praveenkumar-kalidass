571b849c5acbac37af220b83d4c8222c67e1f2b1
Praveenkumar Kalidass <praveen.ece@outlook.com>
2021-01-01 12:49:20 +0530

    Website: Migrate to praveenkumar-kalidass.github.io

--- praveenkumar-kalidass/src/constant/color.js
+++
@@ -1,8 +0,0 @@
-const COLOR = {
-  PRIMARY: '#004688',
-  SECONDARY: '#F7DC4F',
-  WHITE: '#FFFFFF',
-  GRAY: '#F0F0F0',
-};
-
-export default COLOR;
--- praveenkumar-kalidass/src/constant/index.js
+++
@@ -1,2 +0,0 @@
-export { default as COLOR } from './color';
-export { default as TEXT } from './text';
